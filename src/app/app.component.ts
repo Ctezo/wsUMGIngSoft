6a5a52a54a09ec7352ca67dfe4b3b0458570b650
Gmorales-fp <78659428+Gmorales-fp@users.noreply.github.com>
2022-05-21 15:30:36 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CI-CD is active by UMG IngSoft semestre 9o.';
+  title = 'CI-CD is active by UMG IngSoft semestre 9o. v2';
 }
